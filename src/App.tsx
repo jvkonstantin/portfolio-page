73c8fbb95fca2abf4625ee49b05aedeb0b7e7592
kappac0dex <kappac0dex@gmail.com>
2021-05-30 15:14:12 +0300

    |

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,67 @@
 import React from 'react';
 import PicLogo from './img/pfp.jpg';
 import './App.css';
-
-
+import { DiGithubAlt } from "react-icons/di";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaDiscord, FaSpotify } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import {BsPeopleFill} from 'react-icons/bs'
+import SocialTooltip from './utils/comps/socialTooltip'
 
 
 function App() {
   return (
-    <div className="App-header">
-      <div style={{position: "absolute", left: "23px", top:"23px"}}>
-        
-      </div>
-      <div className="width-limiter">
-          <div className="App-disc">
-            <p id="underline" style={{fontSize: "20px", marginBottom: "0"}}>  
-              <a href="https://unnamedsoftware.com/kappa/blog">Blog</a>  <a href="https://unnamedsoftware.com/kappa/portfolio">Portfolio</a>
-            </p>
-            <h1 className="App-title">Kappa C0dex</h1>
-            <code>
-              <p>
+      
+
+      <div className="App-header">
+        <div style={{position: "absolute", left: "23px", top:"23px"}}>
           
-                  <div>This is me. Or is it? <br/></div>
-                  <div>I am a front-end backend React , Python and Javascript developer</div>
-                  <div>Maily based in <a className="Location" href="https://unnamedsoftware.com/locations"> Greece, Athens </a> </div>
-             
-                <div className="Social-links"> 
-              
-                {`[`}
-                  <a href="https://unnamedsoftware.com/kappa/github" style={{marginLeft: "-5px"}} target="__blank"> <i className="fab fa-github" /></a>         
-                  <a href="https://unnamedsoftware.com/kappa/twitter" target="__blank"><i className="fab fa-twitter" /></a> 
-                  <a href="https://unnamedsoftware.com/kappa/discordprofile"><i className="fab fa-discord"></i></a>
-                  <a href="https://unnamedsoftware.com/developers"><i className="fas fa-laptop-code" /></a>           
-                {`]`}
-                </div>
-              </p>
-            </code>
-          </div>     
-            <img src={PicLogo} className="App-logo"  alt="pfp" />
         </div>
-        <footer>
-          <div><h1> Kappa™ • 2021 </h1></div>
-          <div>
-            <h1>
-              <span /> <a href="https://unnamedsoftware.com/kappa/spotify" target="__blank"><i className="fab fa-spotify" /></a> •
-              <span /> <a href="https://unnamedsoftware.com/kappa/contact" target="__blank"><i className="fas fa-envelope" /></a> •
-              <span /> <a href="https://unnamedsoftware.com/kappa/github"  target="__blank"> <i className="fab fa-github" /></a> •
-              <span /> <a href="https://unnamedsoftware.com/kappa/twitter" target="__blank"><i className="fab fa-twitter" /></a> •
-              <span /> <a href="https://unnamedsoftware.com/kappa/discordprofile" target="__blank"><i className="fab fa-discord"></i></a> 
-            </h1>
+        <div className="width-limiter">
+            <div className="App-disc">
+              <p id="underline" style={{fontSize: "20px", marginBottom: "0"}}>  
+                <a href="https://unnamedsoftware.com/kappa/blog">Blog</a>  <a href="https://unnamedsoftware.com/kappa/portfolio">Portfolio</a>
+              </p>
+              <h1 className="App-title">Kappa C0dex</h1>
+              <code>
+                <p>
+            
+                    <div>This is me. Or is it? <br/></div>
+                    <div>I am a front-end / backend React{" (Gatby, Next, Typescript)"} , Python and Java developer</div>
+                    <div>Mainly based in <a className="Location" href="https://unnamedsoftware.com/locations"> Greece, Athens </a> </div>
+              
+                  <div className="Social-links"> 
+                
+                  {`[`}
+                    {/* a data-for="custom-class"
+                  data-tip="hover on me will keep the tooltip" */}
+                    <SocialTooltip><a data-for="custom-class" data-tip="find my projects on GitHub!" href="https://unnamedsoftware.com/members/kappa/github" style={{marginLeft: "-5px"}} target="__blank"> <DiGithubAlt /></a></SocialTooltip>
+                    <SocialTooltip><a data-for="custom-class" data-tip="i tweet things i guess?" href="https://unnamedsoftware.com/members/kappa/twitter" target="__blank"><AiOutlineTwitter /></a></SocialTooltip>
+                    <SocialTooltip><a data-for="custom-class" data-tip="join the discord"  href="https://unnamedsoftware.com/members/kappa/discordprofile"><FaDiscord /></a></SocialTooltip>
+                    <SocialTooltip><a data-for="custom-class" data-tip="my friends"  href="https://unnamedsoftware.com/developers" style={{marginLeft: "5px"}}><BsPeopleFill /></a></SocialTooltip>
+                  {`]`}
+                  </div>
+                </p>
+              </code>
+            </div>     
+              <img src={PicLogo} className="App-logo"  alt="pfp" />
           </div>
-        </footer>
-    </div>
+          <footer>
+            <div><h1> Kappa™ • 2021 </h1></div>
+            <div className="App-Footer-Link">
+                               
+                
+        
+                <span /> <a href="https://unnamedsoftware.com/kappa/spotify" target="__blank"><FaSpotify/></a> <span>•</span>
+                <span /> <a href="https://unnamedsoftware.com/kappa/contact" target="__blank"><IoMdMail/></a> <span>•</span>
+                <span /> <a href="https://unnamedsoftware.com/kappa/github"  target="__blank"> <DiGithubAlt /></a> <span>•</span>
+                <span /> <a href="https://unnamedsoftware.com/kappa/twitter" target="__blank"><AiOutlineTwitter /></a> <span>•</span>
+                <span /> <a href="https://unnamedsoftware.com/kappa/discordprofile" target="__blank"><FaDiscord /></a> 
+         
+            </div>
+          </footer>
+      </div>
+
   );
 }
 
